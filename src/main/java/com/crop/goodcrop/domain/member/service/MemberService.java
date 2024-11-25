cecf95d376435368d9dddfb46e41d5da55c81d66
siyeon02 <chrysapark@cau.ac.kr>
2024-11-25 16:36:27 +0900

    feat(member-service) : 프로필 조회 서비스 #7 - 프로필 조회 시 사용자 존재 확인 - dto 반환

--- src/main/java/com/crop/goodcrop/domain/member/service/MemberService.java
+++ src/main/java/com/crop/goodcrop/domain/member/service/MemberService.java
@@ -1,8 +1,24 @@
 package com.crop.goodcrop.domain.member.service;
 
+import com.crop.goodcrop.domain.member.dto.request.MemberRequestDto;
+import com.crop.goodcrop.domain.member.dto.response.MemberResponseDto;
+import com.crop.goodcrop.domain.member.entity.Member;
+import com.crop.goodcrop.domain.member.repository.MemberRepository;
+import com.crop.goodcrop.exception.ErrorCode;
+import com.crop.goodcrop.exception.ResponseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberResponseDto receiveUserInfo(MemberRequestDto requestDto) {
+        Member member = memberRepository.findById(requestDto.getId())
+                .orElseThrow(()-> new ResponseException(ErrorCode.USER_NOT_FOUND));
+
+        return new MemberResponseDto(member);
+    }
 }
