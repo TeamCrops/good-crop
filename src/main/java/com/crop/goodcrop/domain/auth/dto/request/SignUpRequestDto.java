a3758ddbfa9a418ce793a9bea9aba60b346069ad
JIMINJI1 <mj013031@gmail.com>
2024-11-25 16:11:38 +0900

    feat(auth-dto): 회원가입 dto #3
    - 이메일,비밀번호,닉네임은 필수 입력

--- src/main/java/com/crop/goodcrop/domain/auth/dto/request/SignUpRequestDto.java
+++ src/main/java/com/crop/goodcrop/domain/auth/dto/request/SignUpRequestDto.java
@@ -1,9 +1,26 @@
 package com.crop.goodcrop.domain.auth.dto.request;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Getter
-
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequestDto {
+    @NotBlank(message = "이메일 입력은 필수입니다")
+    @Email
+    private String email;
+    @NotBlank(message = "비밀번호 입력은 필수입니다")
+    private String password;
+    @NotBlank(message = "닉네임 입력은 필수입니다")
+    private String nickname;
+    @Nullable
+    private LocalDate birth;
 
 }
