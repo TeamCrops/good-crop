c579ea4f478e02e17325e7a53fee9fc9e6105333
hwk9407 <hwk__@naver.com>
2024-11-25 16:49:52 +0900

    feat(Review): 단일 상품 조회 기능 추가 #4
    - 없는 상품을 찾을 시 예외 처리
    - 리뷰가 없는 상품의 경우 평균 평점을 -1을 응답하도록 설정

--- src/main/java/com/crop/goodcrop/domain/product/controller/ProductController.java
+++ src/main/java/com/crop/goodcrop/domain/product/controller/ProductController.java
@@ -1,4 +1,25 @@
 package com.crop.goodcrop.domain.product.controller;
 
+
+import com.crop.goodcrop.domain.product.dto.response.ProductResponseDto;
+import com.crop.goodcrop.domain.product.entity.Product;
+import com.crop.goodcrop.domain.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<ProductResponseDto> retrieveProduct(@PathVariable Long productId) {
+        return ResponseEntity.ok(productService.retrieveProduct(productId));
+    }
 }
--- src/main/java/com/crop/goodcrop/domain/product/service/ProductService.java
+++ src/main/java/com/crop/goodcrop/domain/product/service/ProductService.java
@@ -1,7 +1,33 @@
 package com.crop.goodcrop.domain.product.service;
 
+import com.crop.goodcrop.domain.product.dto.response.ProductResponseDto;
+import com.crop.goodcrop.domain.product.entity.Product;
+import com.crop.goodcrop.domain.product.repository.ProductRepository;
+import com.crop.goodcrop.domain.review.repository.ReviewRepository;
+import com.crop.goodcrop.exception.ErrorCode;
+import com.crop.goodcrop.exception.ResponseException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final ReviewRepository reviewRepository;
+
+    public ProductResponseDto retrieveProduct(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResponseException(ErrorCode.PRODUCT_NOT_FOUND));
+        Double avgScore = Optional.ofNullable(reviewRepository.findAverageScoreByProduct(product))
+                .orElse(-1.0);
+
+        return new ProductResponseDto(
+                product.getId(),
+                product.getName(),
+                product.getPrice(),
+                avgScore);
+    }
 }
