f268da848c637ecc05d788bcdd8a0b3292733931
eun0 <shinely12@gmail.com>
2024-11-25 19:47:05 +0900

    feat(review-create): 리뷰 등록 #6
    - Spring Security 적용 전, 리뷰 등록

--- src/main/java/com/crop/goodcrop/domain/review/controller/ReviewController.java
+++ src/main/java/com/crop/goodcrop/domain/review/controller/ReviewController.java
@@ -1,12 +1,31 @@
 package com.crop.goodcrop.domain.review.controller;
 
+import com.crop.goodcrop.domain.review.dto.request.ReviewRequestDto;
+import com.crop.goodcrop.domain.review.dto.response.ReviewResponseDto;
 import com.crop.goodcrop.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/products/{productId}/reviews")
 public class ReviewController {
 
     private final ReviewService reviewService;
+
+    @PostMapping("")
+    public ResponseEntity<ReviewResponseDto> writeReview(
+//            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestParam Long memberId,
+            @PathVariable Long productId,
+            @RequestBody ReviewRequestDto reviewRequestDto
+    ) {
+//        Long memberId = userDetails.getMember().getId();
+        ReviewResponseDto reviewResponseDto = reviewService.createReview(memberId, productId, reviewRequestDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(reviewResponseDto);
+    }
 }
