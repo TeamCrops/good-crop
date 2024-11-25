4dad915895e4e5d6b27fdb26b239fe1d7df547ec
JIMINJI1 <mj013031@gmail.com>
2024-11-25 20:11:40 +0900

    feat(security): 사용자 인증을 위한 UserDetailsImpl 및 UserDetailsServiceImpl 추가 #9
    - Spring Security의 UserDetails 인터페이스 구현
      - Member 엔티티를 기반으로 사용자 인증 정보 반환
    - UserDetailsService 인터페이스 구현
      - 이메일로 사용자 조회 기능(loadUserByUsername)
    - MemberRepository와 연동하여 인증 처리

--- src/main/java/com/crop/goodcrop/security/entity/UserDetailsImpl.java
+++ src/main/java/com/crop/goodcrop/security/entity/UserDetailsImpl.java
@@ -1,4 +1,58 @@
 package com.crop.goodcrop.security.entity;
 
-public class UserDetailsImpl{
+import com.crop.goodcrop.domain.member.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class UserDetailsImpl implements UserDetails {
+    private final Member member;
+
+    public UserDetailsImpl(Member member) {
+        this.member = member;
+    }
+
+    public Member getUser() {
+        return member;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    // unsername -> email
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    // 사용자의 권한을 GrantedAuthority 로 추상화 및 반환
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // 유저의 권한이 없는 경우 빈 리스트 반환
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
