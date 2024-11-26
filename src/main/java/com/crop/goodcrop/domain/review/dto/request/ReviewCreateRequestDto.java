e5c6c00a1f81ac5eba908bad3d1dc4d0fdc5c4ee
eun0 <shinely12@gmail.com>
2024-11-27 02:24:14 +0900

    feat(review-modify): 리뷰 수정 #21
    - Request DTO 분리
      - 리뷰 등록 Request DTO: ReviewCreateRequestDto
      - 리뷰 수정 Request DTO: ReviewModifyRequestDto
    
    - Request DTO 에 대한 제약 조건 추가

---
+++ src/main/java/com/crop/goodcrop/domain/review/dto/request/ReviewCreateRequestDto.java
@@ -0,0 +1,19 @@
+package com.crop.goodcrop.domain.review.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
+
+@Builder
+@Getter
+public class ReviewCreateRequestDto {
+    // 별점: 최소 1점, 최대 5점
+    @NotBlank
+    @Range(min=1, max=5)
+    private int score;
+
+    @Size(max = 100, message = "리뷰 내용은 최대 100자까지 입력 가능합니다.")
+    private String comment;
+}
