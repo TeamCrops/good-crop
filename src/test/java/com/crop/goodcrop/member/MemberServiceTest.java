47c19defc30a4c20d2b491eade260cead69c436f
siyeon02 <chrysapark@cau.ac.kr>
2024-11-26 22:54:36 +0900

    fix(member-retrieve) : 프로필 조회 테스트 #47 - 오류 수정

--- src/test/java/com/crop/goodcrop/member/MemberServiceTest.java
+++ src/test/java/com/crop/goodcrop/member/MemberServiceTest.java
@@ -5,68 +5,65 @@ import com.crop.goodcrop.domain.member.dto.response.MemberResponseDto;
 import com.crop.goodcrop.domain.member.entity.Member;
 import com.crop.goodcrop.domain.member.repository.MemberRepository;
 import com.crop.goodcrop.domain.member.service.MemberService;
-import com.crop.goodcrop.domain.review.dto.response.ReviewResponseDto;
-import com.crop.goodcrop.domain.review.entity.Review;
-import com.crop.goodcrop.domain.review.service.ReviewService;
+import com.crop.goodcrop.exception.ErrorCode;
 import com.crop.goodcrop.exception.ResponseException;
 import com.crop.goodcrop.security.entity.UserDetailsImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class MemberServiceTest {
 
-    @Autowired
+    @Mock
     private MemberRepository memberRepository;
 
-    @Autowired
+    @InjectMocks
     private MemberService memberService;
+    private Member member;
+    private UserDetailsImpl userDetails;
 
-    @Autowired
+    @Mock
     private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void setUp() {
-        memberService = new MemberService(memberRepository, passwordEncoder);
+        MockitoAnnotations.openMocks(this);
+        member = Member.builder()
+                .id(1L)
+                .email("test@email.com")
+                .nickname("testUser")
+                .password(passwordEncoder.encode("password1234!"))
+                .birth(LocalDate.of(2000, 1, 1))
+                .build();
+        userDetails = new UserDetailsImpl(member);
     }
 
     @Test
     @DisplayName("프로필 조회 - 성공")
     void receiveUserInfo_Success() {
         // Given
-        Member member = memberRepository.save(Member.builder()
-                .id(1L)
-                .email("sparta@email.com")
-                .nickname("sparta")
-                .password("password1234!")
-                .birth(LocalDate.parse("2024-11-26"))
-                .build());
-
-        UserDetailsImpl userDetails = new UserDetailsImpl(member);
-
-        // When
         MemberRequestDto requestDto = MemberRequestDto.builder()
-                .id(member.getId())
+                .id(1L)
                 .build();
 
+        // When
         MemberResponseDto responseDto = memberService.receiveUserInfo(requestDto, userDetails);
 
         // Then
-        assertThat(responseDto).isNotNull();
-        assertThat(responseDto.getId()).isEqualTo(member.getId());
-        assertThat(responseDto.getNickname()).isEqualTo(member.getNickname());
-        assertThat(responseDto.getBirth()).isEqualTo(member.getBirth().toString());
+        assertEquals(member.getId(), responseDto.getId());
+        assertEquals(member.getNickname(), responseDto.getNickname());
+        assertEquals(member.getBirth(), responseDto.getBirth());
 
     }
 
@@ -74,49 +71,34 @@ class MemberServiceTest {
     @DisplayName("프로필 조회 - 실패(ID 불일치)")
     void receiveUserInfo_IdFail() {
         // Given
-        Member member = memberRepository.save(Member.builder()
-                .id(1L)
-                .email("sparta@email.com")
-                .nickname("sparta")
-                .password("password1234!")
-                .birth(LocalDate.parse("2024-11-26"))
-                .build());
-
-        UserDetailsImpl userDetails = new UserDetailsImpl(member);
-
-        // When
         MemberRequestDto requestDto = MemberRequestDto.builder()
                 .id(member.getId()+1L)
                 .build();
-
+        // When
         // Then
-        assertThrows(ResponseException.class, () -> {
+        ResponseException exception =  assertThrows(ResponseException.class, () -> {
             memberService.receiveUserInfo(requestDto, userDetails);
         }, "ID가 일치하지 않아야 합니다.");
+
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode(), "예상된 에러코드와 일치해야 합니다.");
 
     }
 
     @Test
     @DisplayName("프로필 조회 - 실패 (사용자 없음)")
     void receiveUserInfo_UserNotFound_Fail() {
-        // Given
-        UserDetailsImpl userDetails = new UserDetailsImpl(Member.builder()
-                .id(1L)
-                .email("nonexistent@email.com")
-                .nickname("nonexistent")
-                .password(passwordEncoder.encode("password1234!"))
-                .birth(LocalDate.parse("2024-11-26"))
-                .build());
 
-        // When
+        // Given
         MemberRequestDto requestDto = MemberRequestDto.builder()
                 .id(999L) // 존재하지 않는 ID
                 .build();
-
+        // When
         // Then
-        assertThrows(ResponseException.class, () -> {
+        ResponseException exception = assertThrows(ResponseException.class, () -> {
             memberService.receiveUserInfo(requestDto, userDetails);
         }, "존재하지 않는 사용자를 조회하려고 하면 예외가 발생해야 합니다.");
+
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode(), "예상된 에러코드와 일치해야 합니다.");
     }
 
     @Test
