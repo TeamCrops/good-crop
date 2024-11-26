f03f0eeb4c00143638cad2e7a02d60f6dbc01a74
siyeon02 <chrysapark@cau.ac.kr>
2024-11-26 15:29:03 +0900

    test(member-retrieve) : 프로필 조회 테스트 #7 - id 불일치 실패 테스트 작성 - 존재하지 않는 멤버 실패 테스트 작성

--- src/test/java/com/crop/goodcrop/member/MemberServiceTest.java
+++ src/test/java/com/crop/goodcrop/member/MemberServiceTest.java
@@ -8,6 +8,7 @@ import com.crop.goodcrop.domain.member.service.MemberService;
 import com.crop.goodcrop.domain.review.dto.response.ReviewResponseDto;
 import com.crop.goodcrop.domain.review.entity.Review;
 import com.crop.goodcrop.domain.review.service.ReviewService;
+import com.crop.goodcrop.exception.ResponseException;
 import com.crop.goodcrop.security.entity.UserDetailsImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -21,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class MemberServiceTest {
@@ -66,6 +68,55 @@ class MemberServiceTest {
         assertThat(responseDto.getNickname()).isEqualTo(member.getNickname());
         assertThat(responseDto.getBirth()).isEqualTo(member.getBirth().toString());
 
+    }
+
+    @Test
+    @DisplayName("프로필 조회 - 실패(ID 불일치)")
+    void receiveUserInfo_IdFail() {
+        // Given
+        Member member = memberRepository.save(Member.builder()
+                .id(1L)
+                .email("sparta@email.com")
+                .nickname("sparta")
+                .password("password1234!")
+                .birth(LocalDate.parse("2024-11-26"))
+                .build());
+
+        UserDetailsImpl userDetails = new UserDetailsImpl(member);
+
+        // When
+        MemberRequestDto requestDto = MemberRequestDto.builder()
+                .id(999L)
+                .build();
+
+        // Then
+        assertThrows(ResponseException.class, () -> {
+            memberService.receiveUserInfo(requestDto, userDetails);
+        }, "ID가 일치하지 않아야 합니다.");
+
+    }
+
+    @Test
+    @DisplayName("프로필 조회 - 실패 (사용자 없음)")
+    void receiveUserInfo_UserNotFound_Fail() {
+        // Given
+        UserDetailsImpl userDetails = new UserDetailsImpl(Member.builder()
+                .id(1L)
+                .email("nonexistent@email.com")
+                .nickname("nonexistent")
+                .password(passwordEncoder.encode("password1234!"))
+                .birth(LocalDate.parse("2024-11-26"))
+                .build());
+
+        // When
+        MemberRequestDto requestDto = MemberRequestDto.builder()
+                .id(999L) // 존재하지 않는 ID
+                .build();
+
+        // Then
+        assertThrows(ResponseException.class, () -> {
+            memberService.receiveUserInfo(requestDto, userDetails);
+        }, "존재하지 않는 사용자를 조회하려고 하면 예외가 발생해야 합니다.");
     }
 
     @Test
