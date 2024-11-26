f7c964969f4b44b39e1152cbf9228044cb7e4c9f
eun0 <shinely12@gmail.com>
2024-11-27 02:37:15 +0900

    feat(review-modify): 리뷰 수정 #21
    - Spring Security 적용 전, 리뷰 수정

--- src/main/java/com/crop/goodcrop/domain/review/service/ReviewService.java
+++ src/main/java/com/crop/goodcrop/domain/review/service/ReviewService.java
@@ -5,7 +5,8 @@ import com.crop.goodcrop.domain.member.entity.Member;
 import com.crop.goodcrop.domain.member.repository.MemberRepository;
 import com.crop.goodcrop.domain.product.entity.Product;
 import com.crop.goodcrop.domain.product.repository.ProductRepository;
-import com.crop.goodcrop.domain.review.dto.request.ReviewRequestDto;
+import com.crop.goodcrop.domain.review.dto.request.ReviewCreateRequestDto;
+import com.crop.goodcrop.domain.review.dto.request.ReviewModifyRequestDto;
 import com.crop.goodcrop.domain.review.dto.response.ReviewResponseDto;
 import com.crop.goodcrop.domain.review.entity.Review;
 import com.crop.goodcrop.domain.review.repository.ReviewRepository;
@@ -16,9 +17,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import static com.crop.goodcrop.exception.ErrorCode.PRODUCT_NOT_FOUND;
-import static com.crop.goodcrop.exception.ErrorCode.USER_NOT_FOUND;
+import static com.crop.goodcrop.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -28,7 +29,9 @@ public class ReviewService {
     private final ProductRepository productRepository;
     private final ReviewRepository reviewRepository;
 
-    public ReviewResponseDto createReview(Long memberId, Long productId, ReviewRequestDto reviewRequestDto) {
+    public ReviewResponseDto createReview(Long memberId, Long productId, ReviewCreateRequestDto reviewRequestDto) {
+        // TODO
+        //  - 이미 필터에서 확인한 부분으로 삭제
         // DB에 존재하는 유저인지 확인
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new ResponseException(USER_NOT_FOUND));
@@ -51,5 +54,28 @@ public class ReviewService {
 
         Page<Review> reviews = reviewRepository.findByProductId(productId, pageable);
         return PageResponseDto.of(reviews.toList(), pageable, reviews.getTotalPages());
+    }
+
+    @Transactional
+    public ReviewResponseDto modifyReview(Long memberId, Long productId, Long reviewId, ReviewModifyRequestDto requestDto) {
+        // DB에 존재하는 상품인지 확인
+        productRepository.findById(productId)
+                .orElseThrow(() -> new ResponseException(PRODUCT_NOT_FOUND));
+        // DB에 존재하는 리뷰인지 확인
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ResponseException(REVIEW_NOT_FOUND));
+
+        // 리뷰를 수정할 권한이 없는 경우
+        if (!memberId.equals(review.getMember().getId())) {
+            throw new ResponseException(NO_PERMISSION_TO_EDIT);
+        }
+        // 해당 상품의 리뷰가 아닌 경우
+        if (!productId.equals(review.getProduct().getId())) {
+            throw new ResponseException(NOT_A_REVIEW_OF_THE_PRODUCT);
+        }
+
+        review.modify(requestDto.getComment(), requestDto.getScore());
+
+        return ReviewResponseDto.from(review);
     }
 }
