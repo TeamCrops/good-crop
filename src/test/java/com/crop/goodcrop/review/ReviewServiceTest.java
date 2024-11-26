083b68e7911b6dd54830b6bcb84e701ec0402541
eun0 <shinely12@gmail.com>
2024-11-26 12:35:05 +0900

    test(review-retrieve): 리뷰 조회 #18
    - 조회 반환 객체가 Page 객체에서 PageResponseDto 로 변환됨에 따라, 테스트 코드 리팩토링

--- src/test/java/com/crop/goodcrop/review/ReviewServiceTest.java
+++ src/test/java/com/crop/goodcrop/review/ReviewServiceTest.java
@@ -1,5 +1,6 @@
 package com.crop.goodcrop.review;
 
+import com.crop.goodcrop.domain.common.dto.PageResponseDto;
 import com.crop.goodcrop.domain.member.entity.Member;
 import com.crop.goodcrop.domain.member.repository.MemberRepository;
 import com.crop.goodcrop.domain.product.entity.Product;
@@ -16,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -102,13 +105,13 @@ class ReviewServiceTest {
         }
 
         // When
-        Page<ReviewResponseDto> responses = reviewService.retrieveReviews(product.getId(), 1);
+        PageResponseDto<Review> responses = reviewService.retrieveReviews(product.getId(), 1);
 
         // Then
-
         assertThat(responses).isNotNull();
-        assertThat(responses.getContent().size()).isEqualTo(10); // 페이지 크기 확인
-        assertThat(responses.getTotalElements()).isEqualTo(15); // 전체 리뷰 개수 확인
-        assertThat(responses.getContent().get(0).getComment()).isEqualTo("Review 15"); // 가장 최근 리뷰 확인
+        assertThat(responses.getSize()).isEqualTo(10); // 페이지 크기가 10인지 확인
+        assertThat(responses.getTotalPage()).isEqualTo(2); // 총 페이지 개수 확인
+        assertThat(responses.getData().get(0).getComment()).isEqualTo("Review 15"); // 첫 번째 리뷰 내용 확인
+        assertThat(responses.getData().get(9).getComment()).isEqualTo("Review 6"); // 마지막 리뷰 내용 확인
     }
 }
