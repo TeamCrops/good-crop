a2f142365553ec71b01dbdcded016610aae95d90
hwk9407 <hwk__@naver.com>
2024-11-26 14:01:04 +0900

    fix(Review): 단순 충돌 해결 #4

--- src/main/java/com/crop/goodcrop/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/crop/goodcrop/domain/review/repository/ReviewRepository.java
@@ -1,10 +1,15 @@
 package com.crop.goodcrop.domain.review.repository;
 
+import com.crop.goodcrop.domain.product.entity.Product;
 import com.crop.goodcrop.domain.review.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findByProductId(Long productId, Pageable pageable);
+
+    @Query("SELECT AVG(r.score) FROM Review r WHERE r.product = :product")
+    Double findAverageScoreByProduct(Product product);
 }
