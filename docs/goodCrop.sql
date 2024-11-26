d21e714f0fd7381ba53843d38dcdc5bba94a0a09
guguggagga <gaeul79_@naver.com>
2024-11-26 10:52:18 +0900

    feat: Dummy 데이터 적재 - loopSearchHistoryInsert 프로시저 쿼리 sql파일에 추가 - README.md API명세서, ERD추가,

---
+++ docs/goodCrop.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+DROP PROCEDURE IF EXISTS loopSearchHistoryInsert $$
+CREATE PROCEDURE loopSearchHistoryInsert()
+BEGIN
+    DECLARE idx INT DEFAULT 0;
+    declare keyword VARCHAR(10);
+    WHILE idx < 100000
+        DO
+            set @keyword = SUBSTR(MD5(RAND()),1,8);
+INSERT INTO search_history(keyword, member_id, created_at)
+VALUES (@keyword, 1, NOW());
+SET idx = idx + 1;
+END WHILE;
+END$$
+DELIMITER $$
+CALL loopSearchHistoryInsert;
