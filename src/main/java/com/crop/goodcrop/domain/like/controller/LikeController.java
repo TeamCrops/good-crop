b5f068e68735b7cfd04d2542e793a05b2fbf36b3
guguggagga <gaeul79_@naver.com>
2024-11-26 11:22:08 +0900

    fix: 좋아요 security 적용 #5, #14 - 좋아요 추가 security 적용 - 좋아요 취소 security 적용

--- src/main/java/com/crop/goodcrop/domain/like/controller/LikeController.java
+++ src/main/java/com/crop/goodcrop/domain/like/controller/LikeController.java
@@ -2,9 +2,11 @@ package com.crop.goodcrop.domain.like.controller;
 
 import com.crop.goodcrop.domain.like.dto.request.LikeRequestDto;
 import com.crop.goodcrop.domain.like.service.LikeService;
+import com.crop.goodcrop.security.entity.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,9 +17,10 @@ public class LikeController {
 
     @PostMapping("/{productId}")
     public ResponseEntity<Void> createLike(
+            @AuthenticationPrincipal UserDetailsImpl userDetail,
             @PathVariable Long productId,
             @RequestBody LikeRequestDto requestDto) {
-        likeService.createLike(requestDto, productId);
+        likeService.createLike(userDetail, requestDto, productId);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .build();
@@ -25,10 +28,11 @@ public class LikeController {
 
     @DeleteMapping("/{productId}")
     public ResponseEntity<Void> deleteLike(
+            @AuthenticationPrincipal UserDetailsImpl userDetail,
             @PathVariable Long productId,
             @RequestBody LikeRequestDto requestDto
     ) {
-        likeService.deleteLike(requestDto, productId);
+        likeService.deleteLike(userDetail, requestDto, productId);
         return ResponseEntity.noContent().build();
     }
 }
