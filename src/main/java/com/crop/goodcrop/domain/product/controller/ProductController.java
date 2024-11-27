a48654d4c848c097600f1bed4fa6194d1074ec91
hwk9407 <hwk__@naver.com>
2024-11-27 22:15:58 +0900

    feat(Product): ProductController 에서 비회원도 MemberId를 -1을 갖는 예시 추가 #61

--- src/main/java/com/crop/goodcrop/domain/product/controller/ProductController.java
+++ src/main/java/com/crop/goodcrop/domain/product/controller/ProductController.java
@@ -6,9 +6,11 @@ import com.crop.goodcrop.domain.product.dto.response.ProductResponseDto;
 import com.crop.goodcrop.domain.product.service.ProductService;
 import com.crop.goodcrop.exception.ErrorCode;
 import com.crop.goodcrop.exception.ResponseException;
+import com.crop.goodcrop.security.entity.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -28,12 +30,15 @@ public class ProductController {
 
     @GetMapping("v1/products")
     public ResponseEntity<PageResponseDto<ProductResponseDto>> searchProducts(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
             @RequestParam String keyword,
             @RequestParam(defaultValue = "0", required = false) int minPrice,
             @RequestParam(defaultValue = "false") boolean isTrend,
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size
             ) {
+        Long memberId = (userDetails != null) ? userDetails.getUser().getId() : -1;
+        System.out.println("memberId: " + memberId);
 
         // keyword가 공백 또는 빈 문자열인 경우 예외 처리
         if (keyword.trim().isEmpty()) {
