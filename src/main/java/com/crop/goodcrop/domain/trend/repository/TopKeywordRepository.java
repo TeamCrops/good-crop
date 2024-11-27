619deac08dcc52ede2942f7bac2e62888481ed1b
JIMINJI1 <mj013031@gmail.com>
2024-11-28 01:41:43 +0900

    feat(product-repository): 상품검색 v2(캐시적용) repository 추가 #59
    - 조건부 캐싱(사용자 입력 검색어가 인기검색어와 일치하는지 확인)

--- src/main/java/com/crop/goodcrop/domain/trend/repository/TopKeywordRepository.java
+++ src/main/java/com/crop/goodcrop/domain/trend/repository/TopKeywordRepository.java
@@ -4,4 +4,5 @@ import com.crop.goodcrop.domain.trend.entity.TopKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopKeywordRepository extends JpaRepository<TopKeyword, Long> {
+    boolean existsByKeyword(String keyword);
 }
