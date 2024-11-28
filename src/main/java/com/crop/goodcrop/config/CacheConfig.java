3bd82dbc6d2037f9f73b4bdb9cb45fbc386c8ef6
siyeon02 <chrysapark@cau.ac.kr>
2024-11-28 10:01:43 +0900

    feat(cache) : 기본 세팅 #56 - 캐시 config 설정 - build.gradle 설정

--- src/main/java/com/crop/goodcrop/config/CacheConfig.java
+++ src/main/java/com/crop/goodcrop/config/CacheConfig.java
@@ -1,19 +1,50 @@
 package com.crop.goodcrop.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 @EnableCaching
 @Configuration
 public class CacheConfig {
-    public static final String PRODUCT = "product";
+    //public static final String PRODUCT = "product";
     public static final String SEARCH_HISTORY = "searchHistory";
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager(PRODUCT, SEARCH_HISTORY);
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(5, TimeUnit.MINUTES) // 5분 후 캐시 만료
+                .maximumSize(1000)); // 캐시 최대 크기
+        return cacheManager;
     }
+
+    @Bean
+    public CaffeineCache searchHistoryCache() {
+        return new CaffeineCache("searchHistory", Caffeine.newBuilder()
+                .expireAfterWrite(5, TimeUnit.MINUTES)
+                .maximumSize(100)
+                .build());
+    }
+
+//    @Bean
+//    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+//        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager(SEARCH_HISTORY);
+//        caffeineCacheManager.setCaffeine(caffeine);
+//        return caffeineCacheManager;
+//    }
+//
+//    @Bean
+//    public Caffeine<Object, Object> caffeineConfig() {
+//        return Caffeine.newBuilder()
+//                .expireAfterWrite(5, TimeUnit.MINUTES) // 5분 후 캐시 만료
+//                .maximumSize(10_000); // 캐시 최대 크기
+//    }
 }
